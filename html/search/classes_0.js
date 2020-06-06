5c80723856966592bcb42bf45a1e849c123756ba
homyak-coder <59015806+homyak-coder@users.noreply.github.com>
2020-06-06 14:20:11 +0300

    Delete classes_0.js

--- html/search/classes_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['snakebotcontroller',['SnakeBotController',['../classSnakeBotController.html',1,'']]],
-  ['snakemodel',['SnakeModel',['../classSnakeModel.html',1,'']]],
-  ['snakeusercontroller',['SnakeUserController',['../classSnakeUserController.html',1,'']]],
-  ['snakeview',['SnakeView',['../classSnakeView.html',1,'']]]
-];
