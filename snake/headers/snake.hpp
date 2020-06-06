4f7d845b60616a94920e0cb6e7156fbd8ae9079d
homyak-coder <59015806+homyak-coder@users.noreply.github.com>
2020-06-06 13:53:08 +0300

    Delete snake.hpp

--- snake/headers/snake.hpp
+++
@@ -1,7 +0,0 @@
-#include "snake_model.hpp"
-#include "snake_view.hpp"
-#include "snake_controller.hpp"
-#include "snake_bot.hpp"
-/**
- * @brief вспомогательный файл, чтобы не было 4 инклудов в мэйнах
- */
