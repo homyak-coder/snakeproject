c435390d73a3d5b5dfcc936118b2fcd751a5bb6f
homyak-coder <59015806+homyak-coder@users.noreply.github.com>
2020-06-06 14:19:51 +0300

    Delete all_4.js

--- html/search/all_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['update',['update',['../classSnakeBotController.html#a223f831b61724753055a0c150885606d',1,'SnakeBotController::update()'],['../classSnakeUserController.html#a0a279b93494c12f9a5fb1b1eb7f0bb3f',1,'SnakeUserController::update()'],['../classSnakeModel.html#a7169443680bccfe5e2a6d4022a17fea1',1,'SnakeModel::update()'],['../classSnakeView.html#ac721351fecd214def31e0398df2a852b',1,'SnakeView::update()']]]
-];
